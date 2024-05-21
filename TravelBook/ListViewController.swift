0eca5cff198fdd006c155a0957d43564c1e5d712
nigarixx <alaskarovanigar@gmail.com>
2024-05-21 21:00:51 +0400

    Initial Commit

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Nigar Alaskarova on 07.05.24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
